7417aeb2ac506a48c3fecb3b917c5bc3f5d7507d
PhuocLoc <phuocloc@vn-cubesystem.com>
2022-07-19 15:20:34 +0700

    Fix: metatag 404,401

--- src/components/layouts/common/HeadHtml.tsx
+++ src/components/layouts/common/HeadHtml.tsx
@@ -4,19 +4,27 @@ import { BASE_CONSTANTS } from "../../../constants/base.constants";
 import GoogleTagManager from "./GoogleTagManager";
 
 const HeadHtml = () => {
+  const url: string = BASE_CONSTANTS.BASE_URL;
+  const image: string = `${BASE_CONSTANTS.BASE_URL}/images/ogp.png`;
+
   return (
     <React.Fragment>
       <Head>
-        <title>MAPICON（マピコン）｜消防設備点検アプリ</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="description" content="MAPICON（マピコン）は、消防設備点検を効率化するDXアプリです。MAPICONのアプリを消防設備点検で利用することで、点検報告書を自動作成します。今すぐアプリをダウンロード！"/>
+        <meta property="og:description" content="MAPICON（マピコン）は、消防設備点検を効率化するDXアプリです。MAPICONのアプリを消防設備点検で利用することで、点検報告書を自動作成します。今すぐアプリをダウンロード！" key="og:description"/>
+        <meta property="og:type" content="website" key="og:type"/>
+        <meta property="og:title" content="MAPICON（マピコン）" key="og:title"/>
+        <meta property="og:url" content={url} key="og:url"/>
+        <meta property="og:image" content={image} key="og:image"/>
+        <meta property="og:image:width" content="600" key="og:image:width"/>
+        <meta property="og:image:height" content="315" key="og:image:height"/>
+        <meta property="og:site_name" content="MAPICON" key="og:site_name"/>
+
         <link rel="canonical" href={`${BASE_CONSTANTS.BASE_URL}`}/>
         <link rel="icon" href="/fav.ico" />
-        <meta property="og:description" content="MAPICON（マピコン）は、消防設備点検を効率化するDXアプリです。MAPICONのアプリを消防設備点検で利用することで、点検報告書を自動作成します。今すぐアプリをダウンロード！"/>
-        <meta property="og:type" content="website"/>
-        <meta property="og:title" content="MAPICON（マピコン）"/>
-        <meta property="og:url" content={`${BASE_CONSTANTS.BASE_URL}`}/>
-        <meta property="og:image" content={`${BASE_CONSTANTS.BASE_URL}/images/ogp.png`}/>
-        <meta property="og:site_name" content="MAPICON"/>
+
+        <title>MAPICON（マピコン）｜消防設備点検アプリ</title>
       </Head>
       <GoogleTagManager/>
     </React.Fragment>
--- src/pages/401.tsx
+++ src/pages/401.tsx
@@ -1,15 +1,21 @@
+import React from "react";
+import HeadHtml from "../components/layouts/common/HeadHtml";
+
 const Page401 = () => {
   return (
-    <div className="flex bg-main-teal w-[100vw] h-[100vh] justify-center items-center ">
-      <h1 className="text-[24px] text-white mr-[20px] py-[10px] pr-[23px] border-r-[1px] border-r-white/[.3]">401</h1>
-      <h2 className="text-[14px] text-white">Unauthorized.</h2>
-      <footer className="w-full text-center fixed bottom-0">
-      <div className='w-[100%] bg-grey-background border-[1px] sm:block hidden'></div>
-      <div className="text-[12px] text-white py-[20px]">
-      © 2022 OMY, Inc. All Rights Reserved.
+    <React.Fragment>
+      <HeadHtml/>
+      <div className="flex bg-main-teal w-[100vw] h-[100vh] justify-center items-center ">
+        <h1 className="text-[24px] text-white mr-[20px] py-[10px] pr-[23px] border-r-[1px] border-r-white/[.3]">401</h1>
+        <h2 className="text-[14px] text-white">Unauthorized.</h2>
+        <footer className="w-full text-center fixed bottom-0">
+        <div className='w-[100%] bg-grey-background border-[1px] sm:block hidden'></div>
+        <div className="text-[12px] text-white py-[20px]">
+        © 2022 OMY, Inc. All Rights Reserved.
+        </div>
+        </footer>
       </div>
-      </footer>
-    </div>
+    </React.Fragment>
   )
 };
 export default Page401;
