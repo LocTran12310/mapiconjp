071681bbddc45eeb5240339d406f711205c746e4
PhuocLoc <phuocloc@vn-cubesystem.com>
2022-07-19 17:53:34 +0700

    Update SEO alt

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -9,7 +9,7 @@ const Page404 = () => {
       <HeadHtml />
       <div className="flex flex-col bg-main-teal w-[100vw] h-[100vh] justify-center items-center ">
         <Link href="/">
-          <img src="/images/logo-white.png" alt="" className="max-w-[260px] max-h-[50px]"/>
+          <img src="/images/logo-white.png" alt="logo-white" className="max-w-[260px] max-h-[50px]"/>
         </Link>
         <div className="flex justify-center items-center mt-[40px]">
           <h1 className="text-[24px] text-white mr-[20px] py-[10px] pr-[23px] border-r-[1px] border-r-white/[.3]">404</h1>
